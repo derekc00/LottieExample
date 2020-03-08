89697f7d37ff43098ec2e52d4d306c584963afa1
Derek Chang <derektc1@uci.edu>
2020-03-08 01:56:22 -0800

    Full push

--- LottiePracticev3/ViewController.swift
+++ LottiePracticev3/ViewController.swift
@@ -7,12 +7,44 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var animationContainer: UIView!
+    let starAnimationView = AnimationView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //get reference to the json
+        guard let myFile = Bundle.main.path(forResource: "Mail", ofType: "json")
+        else {
+            print("error getting json")
+            return
+        }
+        
+        starAnimationView.animation = Animation.filepath(myFile)
+        
+        //if this is true, there will be autolayout conflicts
+        starAnimationView.translatesAutoresizingMaskIntoConstraints = false
+        
+        //set the animation to loop
+        starAnimationView.loopMode = .loop
+        
+        //add to animation into container
+        animationContainer.addSubview(starAnimationView)
+        
+        //Pin Animation(childView) to edges of container(parentView)
+        NSLayoutConstraint.activate([
+            starAnimationView.leadingAnchor.constraint(equalTo: animationContainer.leadingAnchor),
+            starAnimationView.trailingAnchor.constraint(equalTo: animationContainer.trailingAnchor),
+            starAnimationView.topAnchor.constraint(equalTo: animationContainer.topAnchor),
+            starAnimationView.bottomAnchor.constraint(equalTo: animationContainer.bottomAnchor)
+        ])
+
+        
+        starAnimationView.play()
     }
 
 

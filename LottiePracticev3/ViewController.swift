b32b7efcdeea796f4de3da9f9771166569311981
Derek Chang <derektc1@uci.edu>
2020-03-08 01:05:05 -0800

    Initial Commit

---
+++ LottiePracticev3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LottiePracticev3
+//
+//  Created by Derek Chang on 3/8/20.
+//  Copyright © 2020 Derek Chang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
